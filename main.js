7caf52dc6f581f262ce1a1894e15590b76ad6ee8
Aladiah <wagner.aladiah@gmail.com>
2022-03-03 19:43:43 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+$("button").on('click', () => {
+    $.get('https://api.adviceslip.com/advice', (slip) => {
+        let advId = JSON.parse(slip).slip.id;
+        let advice = JSON.parse(slip).slip.advice;
+
+        $(".advcard-title span").text(advId);
+        $(".advcard-text span").text(advice);
+    });
+  }); 
